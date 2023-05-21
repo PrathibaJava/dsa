0fce562ff523c0480951eb8f1f0b615b9be1e3c6
Prathiba <javadeveloper.prathiba@gmail.com>
2023-05-21 06:52:12 +0100

    First commit

---
+++ src/main/java/com/mission/dsain6months/Dsain6monthsApplication.java
@@ -0,0 +1,13 @@
+package com.mission.dsain6months;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsain6monthsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsain6monthsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mission/dsain6months/Dsain6monthsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mission.dsain6months;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsain6monthsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
