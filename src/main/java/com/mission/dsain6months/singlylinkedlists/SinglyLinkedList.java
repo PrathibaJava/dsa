ef4245024249f7bd647218a7c3d5de55d717be62
Prathiba <javadeveloper.prathiba@gmail.com>
2023-06-15 09:11:02 +0100

    SinglyLinkedList practice

--- src/main/java/com/mission/dsain6months/singlylinkedlists/SinglyLinkedList.java
+++ src/main/java/com/mission/dsain6months/singlylinkedlists/SinglyLinkedList.java
@@ -89,6 +89,63 @@ public class SinglyLinkedList {
 		return current;
 	}
 	
+	public ListNode deleteLastNode() {
+		if(head==null||head.next==null)
+			return head;
+		ListNode previous=null;
+		ListNode current=head;
+		while(current.next!=null) {
+			previous=current;
+			current=current.next;
+		}
+		previous.next=null;
+		return current;
+	}
+	
+	public ListNode deleteNodeAtPosition(int position) {
+		ListNode previous=head;
+		if(position==1) {
+			head=head.next;
+			return previous;
+		}else {
+			int count=1;
+			while(count<position-1) {
+				count++;
+				previous=previous.next;	
+			}
+			ListNode current=previous.next;
+			previous.next=current.next;
+			return current;
+		}
+	}
+	
+	public boolean searchNode(int data) {
+		if(head==null) {
+			return false;
+		}
+		
+		ListNode current=head;
+		while(current!=null) {
+			if(current.data==data) {
+				return true;
+			}
+			current=current.next;
+		}
+		return false;
+	}
+	
+	public void reverseSinglyLinkedList() {
+		ListNode current=head;
+		ListNode previous=null;
+		while(current.next!=null) {
+			previous=current;
+			current=current.next;
+		}
+		
+		
+		
+	}
+	
 	public static void main(String[] args) {
 		SinglyLinkedList sll=new SinglyLinkedList();
 		sll.head=new ListNode(10);
@@ -120,5 +177,20 @@ public class SinglyLinkedList {
 		
 		System.out.println(sll.deleteFirstNode().data);
 		sll.display();
+		
+		System.out.println(sll.deleteLastNode().data);
+		sll.display();
+		
+		System.out.println(sll.deleteLastNode().data);
+		sll.display();
+		
+		System.out.println(sll.deleteNodeAtPosition(1).data);
+		sll.display();
+		
+		System.out.println(sll.deleteNodeAtPosition(3).data);
+		sll.display();
+		
+		System.out.println(sll.searchNode(10));
+		System.out.println(sll.searchNode(12));
 	}
 }
