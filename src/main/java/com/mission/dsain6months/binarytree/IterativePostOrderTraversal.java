93873e2362a79e15356ec02d2d51b9a9a9b41771
Prathiba <javadeveloper.prathiba@gmail.com>
2023-07-13 20:43:29 +0100

    Binary Tree practice

---
+++ src/main/java/com/mission/dsain6months/binarytree/IterativePostOrderTraversal.java
@@ -0,0 +1,18 @@
+package com.mission.dsain6months.binarytree;
+
+public class IterativePostOrderTraversal {
+	
+	private TreeNode root;
+	
+	private static class TreeNode {
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		
+	}
+	
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/main/java/com/mission/dsain6months/binarytree/LevelOrderTraversal.java
@@ -0,0 +1,10 @@
+package com.mission.dsain6months.binarytree;
+
+public class LevelOrderTraversal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
