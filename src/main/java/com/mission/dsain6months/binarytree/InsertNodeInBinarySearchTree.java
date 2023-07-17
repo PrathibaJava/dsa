beefcd0ffbb37afdf01b149bc5937abbe99e0b31
Prathiba <javadeveloper.prathiba@gmail.com>
2023-07-17 09:49:24 +0100

    BinarySearchTree practice

--- src/main/java/com/mission/dsain6months/binarytree/InsertNodeInBinarySearchTree.java
+++ src/main/java/com/mission/dsain6months/binarytree/InsertNodeInBinarySearchTree.java
@@ -1,9 +1,64 @@
 package com.mission.dsain6months.binarytree;
 
-public class Practice {
+public class InsertNodeInBinarySearchTree {
 
+	private TreeNode root;
+	
+	private static class TreeNode {
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(int data) {
+			this.data=data;
+			this.left=null;
+			this.right=null;
+		}
+	}
+	
+	public TreeNode insertNode(TreeNode root, int data) {
+		if(root==null) {
+			root=new TreeNode(data);
+			return root;
+		}
+		
+		if(data < root.data) {
+			root.left=insertNode(root.left, data);
+		}else {
+			root.right=insertNode(root.right, data);
+		}
+		return root;
+	}
+	
+	/*
+	 * public void levelOrder(TreeNode root) { if(root==null) { return; }
+	 * 
+	 * System.out.print(root.data+" ");
+	 * 
+	 * if(root.left!=null) { System.out.print(root.left.data+" "); }
+	 * 
+	 * if(root.right!=null) { System.out.print(root.right.data+" "); }
+	 * 
+	 * levelOrder(root.left); levelOrder(root.right); }
+	 */
+	
+	public void inOrder(TreeNode root) {
+		if(root==null) {
+			return;
+		}
+		inOrder(root.left);
+		System.out.print(root.data+" ");
+		inOrder(root.right);
+	}
+	
 	public static void main(String[] args) {
-
+		InsertNodeInBinarySearchTree bt=new InsertNodeInBinarySearchTree();
+		bt.root=bt.insertNode(bt.root, 5);
+		bt.root=bt.insertNode(bt.root, 3);
+		bt.root=bt.insertNode(bt.root, 7);
+		bt.root=bt.insertNode(bt.root, 1);
+		
+		bt.inOrder(bt.root);
 	}
 
 }
--- src/main/java/com/mission/dsain6months/binarytree/SearchKeyInBinarySearchTree.java
+++ src/main/java/com/mission/dsain6months/binarytree/SearchKeyInBinarySearchTree.java
@@ -1,10 +1,75 @@
 package com.mission.dsain6months.binarytree;
 
-public class Practice1 {
+public class SearchKeyInBinarySearchTree {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private TreeNode root;
 
+	private static class TreeNode {
+		private int data;
+		private TreeNode left;
+		private TreeNode right;
+
+		public TreeNode(int data) {
+			this.data = data;
+			this.left = null;
+			this.right = null;
+		}
 	}
 
+	public TreeNode searchKey(int key) {
+		return search(root, key);
+	}
+
+	public TreeNode search(TreeNode root, int key) {
+		if (root == null || root.data == key) {
+			return root;
+		}
+
+		if (key < root.data) {
+			return search(root.left, key);
+		} else {
+			return search(root.right, key);
+		}
+	}
+
+	public TreeNode insertNode(TreeNode root, int data) {
+		if (root == null) {
+			root = new TreeNode(data);
+			return root;
+		}
+
+		if (data < root.data) {
+			root.left=insertNode(root.left, data);
+		}else {
+			root.right=insertNode(root.right, data);
+		}
+		return root;
+	}
+	
+	public void inOrder(TreeNode root) {
+		if(root==null) {
+			return;
+		}
+		
+		inOrder(root.left);
+		System.out.print(root.data+" ");
+		inOrder(root.right);
+	}
+
+	public static void main(String[] args) {
+		SearchKeyInBinarySearchTree bt = new SearchKeyInBinarySearchTree();
+		bt.root=bt.insertNode(bt.root, 5);
+		bt.root=bt.insertNode(bt.root, 3);
+		bt.root=bt.insertNode(bt.root, 7);
+		bt.root=bt.insertNode(bt.root, 1);
+		
+		bt.inOrder(bt.root);
+		System.out.println();
+		
+		if(bt.searchKey(10)!=null) {
+			System.out.println("Key found");
+		}else {
+			System.out.println("Key not found");
+		}
+	}
 }
